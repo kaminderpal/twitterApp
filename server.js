d3ed20fe4b2bb71aa6279b938c3bb7e0788946c9
kaminderpal <kaminderpal@livebarn.com>
2019-02-22 23:22:02 -0500

    setup express server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const hbs = require('hbs')
+const expressHandlebars = require('express-handlebars')
+
+
+const app = express();
+
+app.engine(".hbs" , expressHandlebars({ defaultLayout : 'layout',extname : '.hbs' }));
+app.set('view engine','hbs');
+app.use(express.static(__dirname +"/public" ));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.listen(9000,()=>{
+    console.log("Server listening at 9000");
+});
