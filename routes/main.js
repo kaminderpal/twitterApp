7b908d64e888564cc4dbd65ebed5800e6a084ae8
kaminderpal <kaminderpal@livebarn.com>
2019-02-22 23:47:50 -0500

    set up home page

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get("/",(req,res,next)=>{
-    res.render("main/landing page");
+    res.render("main/landing");
 });
 
 module.exports = router;
