e24e82bfef8097cd96bf71ac5ab09ae7f24042d7
kaminderpal <kaminderpal@livebarn.com>
2019-02-23 10:01:53 -0500

    configure express session

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcryptjs = require('bcryptjs')
+const crypto = require('crypto')
+
 
 
 const userSchema = new mongoose.Schema({
@@ -18,5 +21,30 @@ const userSchema = new mongoose.Schema({
         }
     ]
 });
+
+userSchema.pre("save",function(next){
+    var user = this;
+    if(!user.isModified('password')) return next();
+    if(user.password){
+        bcryptjs.genSalt(10,function(err,salt){
+            if(err)return next();
+            bcryptjs.hash(user.password,salt,null,function(err,hash){
+                if(err)return next();
+                user.password = hash;
+                next(err);
+            })
+        });
+    }
+});
+
+userSchema.methods.gravatar = function(size){
+    if(!size) size = 200;
+    if(!this.email) return "https://gravatar.com/avatar/?s="+size + "&d=retro";
+    var md5 = crypto.createHash("md5").update(this.email).digest("hex");
+    return "https://gravatar.com/avatar/" + md5 +  "?s=" +size +  "&d=retro";
+}
+userSchema.methods.comparePassword = function(password){
+    return bcryptjs.comparePassword(password,this.password);
+}
 
 module.exports = mongoose.model('User',userSchema);
--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const hbs = require('hbs')
 const expressHandlebars = require('express-handlebars')
+const compression = require('compression');
 const config = require('./config/secrets');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('express-flash');
 
 
 const app = express();
@@ -13,6 +17,10 @@ mongoose.connect(config.database, {useNewUrlParser: true},function(err){
     if(err) console.log(err);
     console.log("mongo connected")
 })
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+
+
 
 app.engine(".hbs" , expressHandlebars({ defaultLayout : 'layout',extname : '.hbs' }));
 app.set('view engine','hbs');
@@ -20,7 +28,14 @@ app.use(express.static(__dirname +"/public" ));
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(session({
+    secret: config.secret,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection : db, autoReconnect : true  })
+}));
+app.use(flash());
+app.use(compression());
 //request urls.
 const mainRoute = require("./routes/main");
 app.use("/",mainRoute);
