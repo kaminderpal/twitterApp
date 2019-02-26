7361e478fae7a4349d3382593af7f663cf2fdaf5
Kaminderpal Singh <kaminderpal@livebarn.com>
2019-02-26 15:46:38 -0500

    impl, home page after login sucess

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 router.get("/",(req,res,next)=>{
-    res.render("main/landing");
+    if(req.user){
+        res.render("main/home");
+    }else{
+        res.render("main/landing");
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,58 +1,57 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const hbs = require('hbs')
-const expressHandlebars = require('express-handlebars')
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
 const compression = require('compression');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
-const config = require('./config/secrets');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const flash = require('express-flash');
+const path = require('path');
+const config = require('./config/secrets');
 
 
 const app = express();
 
-mongoose.connect(config.database, {useNewUrlParser: true},function(err){
-    if(err) console.log(err);
-    console.log("mongo connected")
+mongoose.connect(config.database, { useNewUrlParser: true }, (err) => {
+    if (err) console.log(err);
+    console.log("mongo connected");
 })
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
-
-
-app.engine(".hbs" , expressHandlebars({ defaultLayout : 'layout',extname : '.hbs' }));
-app.set('view engine','hbs');
-app.use(express.static(__dirname +"/public" ));
-app.use(morgan("dev"));
+app.engine('.hbs', expressHandlebars({ defaultLayout: 'layout', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-    secret: config.secret,
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection : db, autoReconnect : true  })
+  secret: config.secret,
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: db, autoReconnect: true }),
 }));
 app.use(flash());
 app.use(compression());
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(function(req,res,next){
-    res.locals.user = req.user;
-    next();
-})
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 
-//request urls.
-const mainRoute = require("./routes/main");
-const userRoute = require("./routes/user");
+// request urls.
+const mainRoute = require('./routes/main');
+const userRoute = require('./routes/user');
+
 app.use(mainRoute);
 app.use(userRoute);
 
 
-app.listen(9000,()=>{
-    console.log("Server listening at 9000");
+app.listen(9000, () => {
+  console.log('Server listening at 9000');
 });
