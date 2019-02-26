a17cc3a9cb6ae1c336178c9210fb7724175202d1
Kaminderpal Singh <kaminderpal@livebarn.com>
2019-02-26 16:20:05 -0500

    connect http with socket io

--- server.js
+++ server.js
@@ -12,20 +12,20 @@ const flash = require('express-flash');
 const path = require('path');
 const http = require('http');
 const io = require('socket.io');
+const passportSocketio = require('passport.socketio');
 const config = require('./config/secrets');
 
 const app = express();
 
 const server = http.Server(app);
 const socket = io(server);
-
 mongoose.connect(config.database, { useNewUrlParser: true }, (err) => {
     if (err) console.log(err);
     console.log("mongo connected");
 })
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
-
+const sessionStore = new MongoStore({ mongooseConnection: db, autoReconnect: true });
 app.engine('.hbs', expressHandlebars({ defaultLayout: 'layout', extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.use(express.static(path.resolve(__dirname, 'public')));
@@ -36,7 +36,7 @@ app.use(session({
   secret: config.secret,
   resave: false,
   saveUninitialized: false,
-  store: new MongoStore({ mongooseConnection: db, autoReconnect: true }),
+  store: sessionStore,
 }));
 app.use(flash());
 app.use(compression());
@@ -47,6 +47,22 @@ app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
 });
+function onAuthSuccess(data, accept) {
+  console.log('successful connection');
+  accept();
+}
+function onAuthFail(data, message, error, accept) {
+  console.log('failed connection');
+  if (error) accept(new Error(message));
+}
+socket.use(passportSocketio.authorize({
+  cookieParser,
+  key: 'connect.sid',
+  secret: config.secret,
+  store: sessionStore,
+  success: onAuthSuccess,
+  fail: onAuthFail,
+}));
 
 // sockets
 require('./sockets/io')(socket);
--- sockets/io.js
+++ sockets/io.js
@@ -1,5 +1,7 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log('connected');
+    const {user} = socket.request;
+    console.log(user.name);
   });
 };
